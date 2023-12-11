4004515c92b17138d304e11f16765e39288ee6c5
Yong Kim <yhkim4573@gmail.com>
2023-12-10 21:03:54 -0500

    nextauth

--- lib/nextAuth.ts
+++ lib/nextAuth.ts
@@ -42,7 +42,7 @@ export const authOptions: NextAuthOptions = {
       // e.g. domain, username, password, 2FA token, etc.
       // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
+        username: { label: "Username", type: "text", placeholder: "Username" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials, req) {
